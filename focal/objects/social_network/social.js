341adbd5433472d459c28be3c38aa5eb9028de56
Lauren Ashley <la.zaubi@gmail.com>
2022-09-30 11:23:47 -0700

    scaffolded script file for W2 Stretch Social Network Analysis - Part I

---
+++ focal/objects/social_network/social.js
@@ -0,0 +1,61 @@
+const biggestFollower = (data) => {
+  // Return account that follows the most other accounts
+};
+
+const mostPopular = () => {
+  // returns the name of the most popular (most followed) individual
+
+};
+
+const printAll = (data) => {
+  // outputs a list of everyone and for each of them, the names of who they follow and who follows them
+
+};
+
+const unrequitedFollowers = (data) => {
+  // returns a list of names for those who follow someone that doesn't follow them back.
+
+};
+
+/** More Goals **/ 
+// Identify who has the most followers over 30
+// Identify who follows the most people over 30
+// List everyone and their reach (sum of # of followers and # of followers of followers)
+
+// Test Code
+const data = {
+  f01: {
+    name: "Alice",
+    age: 15,
+    follows: ["f02", "f03", "f04"]
+  },
+  f02: {
+    name: "Bob",
+    age: 20,
+    follows: ["f05", "f06"]
+  },
+  f03: {
+    name: "Charlie",
+    age: 35,
+    follows: ["f01", "f04", "f06"]
+  },
+  f04: {
+    name: "Debbie",
+    age: 40,
+    follows: ["f01", "f02", "f03", "f05", "f06"]
+  },
+  f05: {
+    name: "Elizabeth",
+    age: 45,
+    follows: ["f04"]
+  },
+  f06: {
+    name: "Finn",
+    age: 25,
+    follows: ["f05"]
+  }
+};
+
+console.log(printAll(data));
+// console.log(biggestFollower(data));
+// console.log(unrequitedFollowers(data));
